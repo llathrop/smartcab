4f9a17fba40bbaa4dffa58057ad028e3163afbfd
llathrop <llathrop@gmail.com>
2016-04-28 13:55:29 -0500

    feat: move agent functions to jupyter notebook, and set up for random run+log of result. agent.py will be an export from the notebook from now on.

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -1,8 +1,63 @@
+
+# coding: utf-8
+
+# # Teach a smartcab to drive
+# 
+# 
+# ## Setup
+# 
+# You need Python 2.7 and pygame for this project: https://www.pygame.org/wiki/GettingStarted
+# For help with installation, it is best to reach out to the pygame community [help page, Google group, reddit].
+# 
+# 
+
+# In[21]:
+
+# Import what we need, and setup the basic function to run from later.
+
+import math
+import string
+import sys
+import os
 import random
+
+sys.path.append("./smartcab/")
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
-       
+
+def run(agentType=agent.LearningAgent,trials=10, gui=False, deadline=False):
+    """Run the agent for a finite number of trials."""
+
+    # Set up environment and agent
+    e = Environment()  # create environment (also adds some dummy traffic)
+    a = e.create_agent(agentType)  # create agent
+    e.set_primary_agent(a, enforce_deadline=deadline)  # specify agent to track
+    # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
+
+    # Now simulate it
+    sim = Simulator(e, update_delay=0.25, display=gui)  # create simulator (uses pygame when display=True, if available)
+    # NOTE: To speed up simulation, reduce update_delay and/or set display=False
+
+    sim.run(n_trials=trials)  # run for a specified number of trials
+    # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
+
+
+# ## Implement a basic driving agent
+# 
+# Implement the basic driving agent, which processes the following inputs at each time step:
+# 
+# Next waypoint location, relative to its current location and heading,
+# Intersection state (traffic light and presence of cars), and,
+# Current deadline value (time steps remaining),
+# And produces some random move/action (None, 'forward', 'left', 'right'). Don’t try to implement the correct strategy! That’s exactly what your agent is supposed to learn.
+# 
+# Run this agent within the simulation environment with enforce_deadline set to False (see run function in agent.py), and observe how it performs. In this mode, the agent is given unlimited time to reach the destination. The current state, action taken by your agent and reward/penalty earned are shown in the simulator.
+# 
+# In your report, mention what you see in the agent’s behavior. Does it eventually make it to the target location?
+# 
+
+# In[18]:
 
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
@@ -17,12 +72,23 @@ class LearningAgent(Agent):
         self.lastAction      = None
         self.lastWaypoint    = None
         self.next_waypoint   = None
+        self.goal=0
+        self.runs=0
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
         print "----------------------------------------------------------"
         print"RESET, Final state:\n", self.state
+        self.runs+=1
+        try:
+            if self.state[0]>0:
+                print "PASS! {} steps to goal,Goal reached {}times out of {}!".format(self.state[0],self.goal,self.runs)
+                self.goal+=1
+            else:
+                print "FAIL! {} steps to goal,Goal reached {}times out of {}!".format(self.state[0],self.goal,self.runs)
+        except:
+            print "Broke! Goal reached {}times out of {}!".format(self.goal,self.runs)
         print "----------------------------------------------------------"
 
     def update(self, t):
@@ -37,11 +103,11 @@ class LearningAgent(Agent):
           
         # TODO: Select action according to your policy
 
-        if self.lastReward <0:
-            action = self.availableAction[random.randint(0,3)]
-        else:
-            action = self.next_waypoint
-            
+        #if self.lastReward <0:
+        #    action = self.availableAction[random.randint(0,3)]
+        #else:
+        #    action = self.next_waypoint
+        action = self.availableAction[random.randint(0,3)]    
         self.lastAction=action
     
         # Execute action and get reward
@@ -49,25 +115,49 @@ class LearningAgent(Agent):
         self.lastReward=reward
         # TODO: Learn policy based on state, action, reward
 
-        print "LearningAgent.update(): self.state{}, action = {}, reward = {}, next_waypoint = {}".format(self.state, action, reward,self.next_waypoint, )  # [debug]
+        #print "LearningAgent.update(): self.state{}, action = {}, reward = {}, next_waypoint = {}".format(self.state, action, reward,self.next_waypoint, )  # [debug]
 
 
-def run():
-    """Run the agent for a finite number of trials."""
 
-    # Set up environment and agent
-    e = Environment()  # create environment (also adds some dummy traffic)
-    a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
-    # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
+# In[19]:
 
-    # Now simulate it
-    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
-    # NOTE: To speed up simulation, reduce update_delay and/or set display=False
+run(agentType=LearningAgent,trials=2)
 
-    sim.run(n_trials=100)  # run for a specified number of trials
-    # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
 
+# 
+# ## Identify and update state
+# 
+# Identify a set of states that you think are appropriate for modeling the driving agent. The main source of state variables are current inputs, but not all of them may be worth representing. Also, you can choose to explicitly define states, or use some combination (vector) of inputs as an implicit state.
+# 
+# At each time step, process the inputs and update the current state. Run it again (and as often as you need) to observe how the reported state changes through the run.
+
+# ## Implement Q-Learning
+# 
+# Implement the Q-Learning algorithm by initializing and updating a table/mapping of Q-values at each time step. Now, instead of randomly selecting an action, pick the best action available from the current state based on Q-values, and return that.
+# 
+# Each action generates a corresponding numeric reward or penalty (which may be zero). Your agent should take this into account when updating Q-values. Run it again, and observe the behavior.
+# 
+# What changes do you notice in the agent’s behavior?
+# 
+# 
+
+# ## Enhance the driving agent
+# 
+# Apply the reinforcement learning techniques you have learnt, and tweak the parameters (e.g. learning rate, discount factor, action selection method, etc.), to improve the performance of your agent. Your goal is to get it to a point so that within 100 trials, the agent is able to learn a feasible policy - i.e. reach the destination within the allotted time, with net reward remaining positive.
+# 
+# Report what changes you made to your basic implementation of Q-Learning to achieve the final version of the agent. How well does it perform?
+# 
+# Does your agent get close to finding an optimal policy, i.e. reach the destination in the minimum possible time, and not incur any penalties?
+# 
+#  PREVIOUS
+
+# In[20]:
 
 if __name__ == '__main__':
     run()
+
+
+# In[ ]:
+
+#eof
+
