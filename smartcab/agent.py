480dda75963a5d78ef30784af0aa0c64c703c5df
llathrop <llathrop@gmail.com>
2016-04-29 22:17:37 -0500

    fix: backout unuseful attempts toward data visualization in StateAgent

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -137,7 +137,7 @@ run(agentType=RandomAgent,trials=2, deadline=True) #Example of a random run
 # 
 # At each time step, process the inputs and update the current state. Run it again (and as often as you need) to observe how the reported state changes through the run.
 
-# In[41]:
+# In[57]:
 
 class StateAgent(RandomAgent):
     """An agent that learns to drive in the smartcab world."""
@@ -153,26 +153,7 @@ class StateAgent(RandomAgent):
         self.runs=0
         self.features={}
         
-    def reset(self, destination=None):
-        self.planner.route_to(destination)
-        # TODO: Prepare for a new trip; reset any variables here, if required
-        #print"RESET, Final state:\n", self.state
-        self.stateFeature=pd.DataFrame(self.features)
-        print self.stateFeature.T.head(5)
-        
-        self.runs+=1
-        try:
-            if self.state[0]>0:
-                print "PASS! {} steps to goal,Goal reached {} times out of {}!".format(self.state[0],self.goal,self.runs)
-                self.goal+=1
-            else:
-                print "FAIL! {} steps to goal,Goal reached {} times out of {}!".format(self.state[0],self.goal,self.runs)
-                pass
-        except:
-            print "Trial 0 - Goal reached {} times out of {}!".format(self.goal,self.runs)
-            pass
-        print "----------------------------------------------------------"
-        
+    
     def update(self, t):
         # Gather inputs
         self.lastWaypoint = self.next_waypoint
@@ -185,7 +166,6 @@ class StateAgent(RandomAgent):
         inputs['deadline']=deadline
         inputs['next_waypoint']=self.next_waypoint
         self.state= inputs    
-        self.features[len(self.features)]=self.state
 
         # TODO: Select action according to your policy
 
@@ -199,7 +179,7 @@ class StateAgent(RandomAgent):
 print "StateAgent Ready"
 
 
-# In[42]:
+# In[58]:
 
 run(agentType=StateAgent,trials=5)
 
