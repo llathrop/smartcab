bd95063f72c94872a4775e02b68075b6452838ae
llathrop <llathrop@gmail.com>
2016-05-04 17:16:27 -0500

    chore: update agent.py from notebook

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -11,7 +11,7 @@
 # 
 # 
 
-# In[156]:
+# In[341]:
 
 # Import what we need, and setup the basic function to run from later.
 
@@ -73,7 +73,7 @@ print "Environment ready"
 # In your report, mention what you see in the agent’s behavior. Does it eventually make it to the target location?
 # 
 
-# In[210]:
+# In[366]:
 
 class RandomAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
@@ -92,14 +92,12 @@ class RandomAgent(Agent):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
         #print"RESET, Final state:\n", self.state
-
-        print len(self.features), self.steps
         try:
-            if self.features[len(self.features)-1][self.steps] >0: #deadline less than zero
-                print "PASS! {} steps to goal,Goal reached {} times out of {}!".format(self.features[len(self.features)-1][self.steps],self.goal,len(self.features))
+            if self.features[len(self.features)-1][self.steps]['deadline'] >0: #deadline less than zero
                 self.goal+=1 #FIXME - order
+                print "PASS! {} steps to goal,Goal reached {} times out of {}!".format(self.features[len(self.features)-1][self.steps]['deadline'],self.goal,len(self.features))
             else:
-                print "FAIL! {} steps to goal,Goal reached {} times out of {}!".format(self.features[len(self.features)-1][self.steps],self.goal,len(self.features))
+                print "FAIL! {} steps to goal,Goal reached {} times out of {}!".format(self.features[len(self.features)-1][self.steps]['deadline'],self.goal,len(self.features))
                 pass
         except:
             print "Trial 0 - Goal reached {} times out of {}!".format(self.goal,len(self.features))
@@ -116,7 +114,8 @@ class RandomAgent(Agent):
         deadline = self.env.get_deadline(self)
         inputs['deadline']=deadline
         self.state=inputs
-    
+        inputs['deadline']=deadline
+        self.features[len(self.features)-1][self.steps]=inputs
         # TODO: Select action according to your policy
         action = self.availableAction[random.randint(0,3)]    
         
@@ -130,12 +129,12 @@ class RandomAgent(Agent):
 print "RandomAgent ready"
 
 
-# In[211]:
+# In[367]:
 
 out=run(agentType=RandomAgent,trials=2, deadline=False) #Example of a random run, with no deadline 
 
 
-# In[212]:
+# In[368]:
 
 out=run(agentType=RandomAgent,trials=2, deadline=True) #Example of a random run
 
@@ -144,6 +143,8 @@ out=run(agentType=RandomAgent,trials=2, deadline=True) #Example of a random run
 # 
 # When we run an agent with a random action policy, we see that it will move about the board with no pattern, and will eventually reach the destination. If we allow the use of deadlines, we see that the agent rarely reaches the destination in time, although it may still occur.
 
+# ---------------------------------------------------------------
+
 # 
 # ## Identify and update state
 # 
@@ -151,7 +152,7 @@ out=run(agentType=RandomAgent,trials=2, deadline=True) #Example of a random run
 # 
 # At each time step, process the inputs and update the current state. Run it again (and as often as you need) to observe how the reported state changes through the run.
 
-# In[213]:
+# In[369]:
 
 class StateAgent(RandomAgent):
     """An agent that learns to drive in the smartcab world."""
@@ -196,13 +197,13 @@ class StateAgent(RandomAgent):
 print "StateAgent Ready"
 
 
-# In[214]:
+# In[380]:
 
 # run the trials for the state
 stateFeatures=run(agentType=StateAgent,trials=25)
 
 
-# In[215]:
+# In[383]:
 
 # display the feedback from the prior run
 import matplotlib.pyplot as plt
@@ -234,7 +235,7 @@ for f in stateFeatures:
     fig.show()
 
 
-# In[216]:
+# In[384]:
 
 plt.plot (all_deadlines)
 plt.ylabel('Deadline')
@@ -243,7 +244,7 @@ plt.title("Deadline per Run")
 plt.show()
 
 
-# ### Identify and update state - discussion.
+# ### Identify and update state - Discussion.
 # When we sense our environment, we perceive 4 variables, with several possible states These include: left, light, next_waypoint, oncoming, and right. We can see right away that light and next_waypoint contains new information at every poll, while the others usually have no value. 
 # 
 # It's not readily apparent that the direction of travel information of the other cars (described by left/right/oncoming) is relevant to our agent. A case could be made to remove the direction information, and only retain information about another car being present at the light. This would have the benefit of reducing the number of possible states, increasing the speed of the agent. This may be a valuable approach in resource constrained environments. 
@@ -253,6 +254,8 @@ plt.show()
 # In the interest of correctness, we will choose to use the state as returned from the sensor, with the addition of the next_waypoint.
 # 
 # While I have tracked the deadline, it is not apparent that it will provide useful information to the agent. It is useful to see note that the agent does not see any usefull increase in the deadline value yet. We may expect this to adapt as we implement learning.
+
+# ---------------------------------------------------------------
 
 # ## Implement Q-Learning
 # 
@@ -264,6 +267,61 @@ plt.show()
 # 
 # 
 
+# In[385]:
+
+class BasicLearningAgent(RandomAgent):
+    """An agent that learns to drive in the smartcab world."""
+
+    def __init__(self, env):
+        super(BasicLearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
+        self.color = 'red'  # override color
+        self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
+        # TODO: Initialize any additional variables here
+        self.availableAction = [None, 'forward', 'left', 'right']   
+        self.next_waypoint   = None
+        self.goal=0
+        self.steps=0
+        self.features=[]
+        
+    def update(self, t):
+        # Gather inputs
+        self.steps+=1
+        
+        self.lastWaypoint = self.next_waypoint
+        self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
+        inputs = self.env.sense(self)
+        
+        deadline = self.env.get_deadline(self)
+        
+        # TODO: Update state
+        
+        inputs['next_waypoint']=self.next_waypoint
+        self.state= inputs    
+        inputs['deadline']=deadline
+        self.features[len(self.features)-1][self.steps]=inputs
+        # TODO: Select action according to your policy
+
+        action = self.availableAction[random.randint(0,3)]    
+    
+        # Execute action and get reward
+        reward = self.env.act(self, action)
+        # TODO: Learn policy based on state, action, reward
+
+        #print "LearningAgent.update(): self.state{}, action = {}, reward = {}, next_waypoint = {}".format(
+        #                                        self.state, action, reward,self.next_waypoint, )  # [debug]
+print "BasicLearningAgent Ready"
+
+
+# In[387]:
+
+# run the trials for the state
+basicLearnFeatures=run(agentType=BasicLearningAgent,trials=5)
+
+
+# ### Implement Q-Learning - Discussion
+
+# ---------------------------------------------------------------
+
 # ## Enhance the driving agent
 # 
 # Apply the reinforcement learning techniques you have learnt, and tweak the parameters (e.g. learning rate, discount factor, action selection method, etc.), to improve the performance of your agent. Your goal is to get it to a point so that within 100 trials, the agent is able to learn a feasible policy - i.e. reach the destination within the allotted time, with net reward remaining positive.
@@ -273,6 +331,16 @@ plt.show()
 # Does your agent get close to finding an optimal policy, i.e. reach the destination in the minimum possible time, and not incur any penalties?
 # 
 #  PREVIOUS
+
+# In[ ]:
+
+
+
+
+# ## Enhance the driving agent - Discussion
+# 
+
+# ---------------------------------------------------------------
 
 # In[7]:
 
